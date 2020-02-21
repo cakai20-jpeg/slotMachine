cbf4ff2cf0eb3a54df12a1555dadfd2df148673c
carson <cakai20@punahou.edu>
2020-02-21 18:17:49 +0000

    adding a ; to most of my code since i forgot to add them as i wrote it initially, also re-doing wheel system and how to determine if someone wins

---
+++ Machine.java
@@ -0,0 +1,18 @@
+public class Machine
+{
+    //instance variables
+    int sides;
+    // constructor
+    public Machine()
+    {
+        outComes = 9;
+    }
+   public int roll()
+   {
+    //pull rolls 3 each numnber = different outcome 
+      //outcomes are diamond lemo watermellon heart seven bell horse shoe cherry bar
+       return (int)(Math.random() * 9) + 1
+       
+      
+       
+}
