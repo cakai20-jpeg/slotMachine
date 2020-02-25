7b2c96886da44b7ba8ed22e2ae4256b8655e45a3
carson <cakai20@punahou.edu>
2020-02-25 18:11:20 +0000

    fixing errors, all same error since lots of code is repeated

--- slotMachine.java
+++ slotMachine.java
@@ -17,7 +17,7 @@ public class DiceGame
       Scanner bet = new Scanner(System.in);
       System.out.println("Please enter bet ammount");
       double betAmmount = bet.nextDouble();
-      creddits = creddits - betAmmount
+      creddits = creddits - betAmmount;
       
       System.out.println("pres any key to spin");
       bet.nextLine();
@@ -27,203 +27,208 @@ public class DiceGame
       int roll1 = slot1.roll();
       int roll2 = slot2.roll();
       int roll3 = slot3.roll();
-      
-//wheel 1
-       if roll1 == 1 ;
+    if 
+       (roll1 == 1);
        return roll1 = "lemon";
+       }
        
-       else if roll1 == 2;
+       else if (roll1 == 2);
        return roll1 = "diamond";
        
-       else if roll1 == 3 ;
+       else if (roll1 == 3);
        return roll1 = "seven";
        
-       else if roll1 == 4 ;
+       else if (roll1 == 4);
        return roll1 = "horse shoe";
        
-       else if roll1 == 5;
+       else if (roll1 == 5);
        return roll1 = "bars";
        
-       else if roll1 == 6;
+       else if (roll1 == 6);
        return roll1 = "cherry";
        
-       else if roll1 == 7 ;
+       else if (roll1 == 7);
        return roll1 = "heart";
        
-       else if roll1 == 8 ;
+       else if (roll1 == 8);
        return roll1 = "bell";
        
-       else if roll1 == 9; 
+       else if (roll1 == 9); 
        return roll1 = "bell";
+         }
 //wheel 2
 
-       if roll2 == 1 ;
+       if 
+       
+      (roll2 == 1);
        return roll2 = "lemon";
        
-       else if roll2 == 2;
+       else if (roll2 == 2);
        return roll2 = "diamond";
        
-       else if roll2 == 3 ;
+       else if (roll2 == 3);
        return roll2 = "seven";
        
-       else if roll2 == 4 ;
+       else if (roll2 == 4);
        return roll2 = "horse shoe";
        
-       else if roll2 == 5;
+       else if (roll2 == 5);
        return roll2 = "bars";
        
-       else if roll2 == 6;
+       else if (roll2 == 6);
        return roll2 = "cherry";
        
-       else if roll2 == 7 ;
+       else if (roll2 == 7);
        return roll2 = "heart";
        
-       else if roll2 == 8 ;
+       else if (roll2 == 8);
        return roll2 = "bell";
        
-       else if roll2 == 9; 
+       else if (roll2 == 9); 
        return roll2 = "bell";
-       
+       }
  //wheel 3
 
-       if roll3 == 1 ;
+       if (roll3 == 1);
        return roll3 = "lemon";
        
-       else if roll3 == 2;
+       else if (roll3 == 2);
        return roll3 = "diamond";
        
-       else if roll3 == 3 ;
+       else if (roll3 == 3);
        return roll3 = "seven";
        
-       else if roll3 == 4 ;
+       else if (roll3 == 4);
        return roll3 = "horse shoe";
        
-       else if roll3 == 5;
+       else if (roll3 == 5);
        return roll3 = "bars";
        
-       else if roll3 == 6;
+       else if (roll3 == 6);
        return roll3 = "cherry";
        
-       else if roll3 == 7 ;
+       else if (roll3 == 7);
        return roll3 = "heart";
        
-       else if roll3 == 8 ;
+       else if (roll3 == 8);
        return roll3 = "bell";
        
-       else if roll3 == 9; 
+       else if (roll3 == 9); 
        return roll3 = "bell";
       
       
       // game winning 
-      if  roll1 == roll2 == roll3;
+      if  (roll1 == roll2 == roll3);
       
       return 
           
       // multiplyer for winning 
      
       // all fruits 3.5x
-      multiplyer = 3.5;
+     double multiplyer = 3.5;
       
         // seven 20x
-        if slot1 == "seven";
+        if (slot1 == "seven");
         multiplyer = 20;
         
         //bars 15x
-        else if slot1 == "bars";
-        multiplyer = 15;
+        else if (slot1 == "bars");
+       double multiplyer = 15;
         
         //bell 10x
-        else if slot1 == "bell";
-        multiplyer = 10;
+        else if (slot1 == "bell");
+      double multiplyer = 10;
         
         //horse shoe 7.5x
-        else if slot1 == "horse shoe";
-        multiplyer = 7.5;
+        else if (slot1 == "horse shoe");
+       double multiplyer = 7.5;
         
         //hearts 5x 
-        else if slot1 == "heart";
-        multiplyer = 5;
+        else if (slot1 == "heart");
+       double multiplyer = 5;
       
       System.out.println("Congragulations! you just won" + betAmmount * multiplyer + "creddits!");
       creddits = (betAmmount * multiplyer) + creddits;
       
-      else if roll1 == roll2;
+      else if (roll1 == roll2);
       
         return 
          // seven 20x
-         multiplyer = 1.5;
+        public double multiplyer = 1.5;
          
-        if slot1 == "seven";
-        multiplyer = 10;
+        if (slot1 == "seven");
+        return multiplyer = 10;
         
         //bars 15x
-        else if slot1 == "bars";
-        multiplyer = 7.5;
+        else if (slot1 == "bars");
+        return multiplyer = 7.5;
         
         //bell 10x
-        else if slot1 == "bell";
-        multiplyer = 5;
+        else if (slot1 == "bell");
+        return multiplyer = 5;
         
         //horse shoe 7.5x
-        else if slot1 == "horse shoe";
-        multiplyer = 3.5;
+        else if (slot1 == "horse shoe");
+        return multiplyer = 3.5;
         
         //hearts 5x 
-        else if slot1 == "heart";
-        multiplyer = 2;
+        else if (slot1 == "heart");
+        return multiplyer = 2;
       
         
-      else if roll1 == roll3;
+      else if (roll1 == roll3);
       
        return 
          // seven 20x
          multiplyer = 1.5;
          
-        if slot1 == "seven";
-        multiplyer = 10;
+        if (slot1 == "seven");
+        return multiplyer = 10;
         
         //bars 15x
-        else if slot1 == "bars";
-        multiplyer = 7.5;
+        else if (slot1 == "bars");
+        return multiplyer = 7.5;
         
         //bell 10x
-        else if slot1 == "bell";
-        multiplyer = 5;
+        else if (slot1 == "bell");
+        return multiplyer = 5;
         
         //horse shoe 7.5x
-        else if slot1 == "horse shoe";
-        multiplyer = 3.5;
+        else if (slot1 == "horse shoe");
+       return multiplyer = 3.5;
         
         //hearts 5x 
-        else if slot1 == "heart";
-        multiplyer = 2;
+        else if (slot1 == "heart");
+        return multiplyer = 2;
       
-      else if roll2 == roll3;
+      else if (roll2 == roll3);
       
        return 
          // seven 20x
          multiplyer = 1.5;
          
-        if slot2 == "seven";
-        multiplyer = 10;
+        if (slot2 == "seven");
+        return multiplyer = 10;
         
         //bars 15x
-        else if slot2 == "bars";
-        multiplyer = 7.5;
+        else if (slot2 == "bars");
+        return multiplyer = 7.5;
         
         //bell 10x
-        else if slot2 == "bell";
-        multiplyer = 5;
+        else if (slot2 == "bell");
+        return multiplyer = 5;
         
         //horse shoe 7.5x
-        else if slot2 == "horse shoe";
-        multiplyer = 3.5;
+        else if (slot2 == "horse shoe");
+        return multiplyer = 3.5;
         
         //hearts 5x 
-        else if slot2 == "heart";
-        multiplyer = 2;
+        else if (slot2 == "heart");
+        return multiplyer = 2;
       // dont win 
       else 
       return "better luck next time";
  
+  }   
+//wheel 1
   }
